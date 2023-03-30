4e82e26d682af3ded698d3f5ddb9140961c35045
jaycedowell <jayce.dowell@gmail.com>
2023-03-29 20:56:49 -0600

    Picking this up again with Kalman filtering and a lock on the "full bin" state.

--- BarrelSpy.ino
+++ BarrelSpy.ino
@@ -6,143 +6,22 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(40, 6, NEO_GRB + NEO_KHZ800);
 
-// Global state variables
-float refPixelLevel, refPixelLevelOld;
-unsigned long refLastChanged;
-
-// Setup the serial port and NeoPixels, and create the initial monitoring
-// state.
-void setup() {
-  // Set for a baud rate of 9,600
-  Serial.begin(9600);
-  
-  // Set up the NeoPixels so that all are off
-  strip.begin();
-  strip.show();
-  
-  // Power on NeoPixel test sequence - red, green, blue, and off
-  colorWipe(strip.Color(25,  0,  0), 50); // Red
-  colorWipe(strip.Color( 0, 25,  0), 50); // Green
-  colorWipe(strip.Color( 0,  0, 25), 50); // Blue
-  colorWipe(strip.Color( 0,  0,  0), 50); // Off
-  
-  // Power light
-  strip.setPixelColor(7, strip.Color(0, 10, 6));
-  
-  // Set the initial state
-  refPixelLevel = -1;
-  refPixelLevelOld = -2;
-  refLastChanged = 0;
-}
-
-void loop() {
-  // Loop control
-  uint16_t i, j, k;
-  
-  // Read the current voltages for the reference and the sensor
-  float refVoltage, sensorVoltage;
-  refVoltage = 5.0*analogRead(A0)/1023.0;
-  sensorVoltage = 5.0*analogRead(A1)/1023.0;
-  
-  // Convert the voltages to distances
-  float refD, sensorD;
-  refD = voltage2distance(refVoltage);
-  sensorD = voltage2distance(sensorVoltage);
-  
-  // Convert the refence distance to a pixel count and clamp it to the 1 
-  // to 8 range
-  refPixelLevel = ( 8 * (80 - refD) / (80 - 10) );
-  refPixelLevel = round( refPixelLevel*2.0 ) / 2.0;
-  if( refPixelLevel < 0.0 ) {
-    refPixelLevel = 0.0;
-  }
-  if( refPixelLevel > 8.0 ) {
-    refPixelLevel = 8.0;
-  }
-  
-  // Part 1 - Has the reference distance changed since the last polling?
-  if( fabs(refPixelLevel - refPixelLevelOld) > 0.75 || millis() - refLastChanged < 4000 ) {
-    /* Reference mode indicator */
-    setPixelColor(39, strip.Color(6, 0, 10));
-    
-    /* Level scales */
-    for(i=8; i<16; i++) {
-      setPixelColor(i,    strip.Color(3*(i-8)+1, 0, 0));
-      setPixelColor(i+16, strip.Color(3*(i-8)+1, 0, 0));
-    }
-    
-    /* The reference level indicator */
-    int frac;
-    for(i=16; i<24; i++) {
-      if( i-16 < refPixelLevel-1 ) {
-        setPixelColor(i, strip.Color(12, 12, 0));
-      } else if( i-16 <= refPixelLevel ) {
-        frac = (int) ((refPixelLevel-i+16)*12);
-        setPixelColor(i, strip.Color(frac, frac, 0));
-      } else {
-        setPixelColor(i, strip.Color(0, 0, 0));
-      }
-    }
-    strip.show();
-    delay(50);
-    
-    /* Update the state */
-    if( refPixelLevel != refPixelLevelOld ) {
-      refLastChanged = millis();
-    }
-    refPixelLevelOld = refPixelLevel;
-  } else {
-    // Clear the pixels to get ready for the next iteration
-    for(i=0; i<strip.numPixels(); i++){
-      setPixelColor(i, strip.Color(0, 0, 0));
-    }
-    strip.show();
-    delay(50);
-  }
-  
-  // Part 2 - Should a notification be displayed?
-  if( sensorD <= refD ) {
-    for(k=0; k<5; k++) {
-      /* Fade from off to white */
-      for(j=0; j<30; j++) {
-        for(i=0; i<strip.numPixels(); i++){
-          strip.setPixelColor(i, strip.Color(j, j, j));
-        }
-        strip.show();
-        delay(50);
-      }
-      
-      /* All yellow */
-      for(i=0; i<strip.numPixels(); i++){
-        strip.setPixelColor(i, strip.Color(30, 19, 0));
-      }
+// Fill the dots one after the other with a color - from the AdaFruit 
+// NeoPixel Library
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels()/2; i++) {
+      strip.setPixelColor(i, c);
+      strip.setPixelColor(strip.numPixels()-1-i, c);
       strip.show();
-      delay(100);
-      
-      /* All white */
-      for(i=0; i<strip.numPixels(); i++) {
-        strip.setPixelColor(i, strip.Color(30, 30, 30));
-      }
-      strip.show();
-      delay(100);
-    }
-    
-    /* All off plus the power light */
-    for(i=0; i<strip.numPixels(); i++){
-      strip.setPixelColor(i, strip.Color(0, 0, 0));
-    }
-    strip.setPixelColor(7, strip.Color(0, 10, 6));
-    strip.show();
-    delay(50);
+      delay(wait);
   }
-  
 }
 
 // Given a voltage between 0 and 3 V, convert it to a distance in cm 
 // assuming a Sharp GP2Y0A21YK0F sensor.  This function uses a quadratic
 // fit to the voltage vs. inverse distance figure shown in the sensor 
 // data sheet.
-float voltage2distance(float voltage) {
+float voltage_to_distance(float voltage) {
   float inverseD;
   
   // Clamp the voltage to the 0 to 3 V range
@@ -163,36 +42,96 @@ float voltage2distance(float voltage) {
   return 1.0/inverseD;
 }
 
-void setPixelColor(uint16_t n, uint32_t c) {
-  if( n != 7 ) {
-    strip.setPixelColor(n, c);
-  }
+void setup() {
+  // Set up the NeoPixels so that all are off
+  strip.begin();
+  strip.show();
+  
+  // Power on NeoPixel test sequence - red, green, blue, and off
+  colorWipe(strip.Color(25,  0,  0), 50); // Red
+  colorWipe(strip.Color( 0, 25,  0), 50); // Green
+  colorWipe(strip.Color( 0,  0, 25), 50); // Blue
+  colorWipe(strip.Color( 0,  0,  0), 50); // Off
+
+  // Dim the display (for now)
+  strip.setBrightness(32);
 }
 
-// Fill the dots one after the other with a color - from the AdaFruit 
-// NeoPixel Library
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels()/2; i++) {
-      strip.setPixelColor(i, c);
-      strip.setPixelColor(strip.numPixels()-1-i, c);
-      strip.show();
-      delay(wait);
-  }
-}
+// Kalman filtering setup
+int sensor, ref;
+float Q = 3e-4;
+float R = 5*5;
+float x = 100, xm1, P = 100, Pm1;
+float value, K;
 
-// Theatre-style crawling lights - from the AdaFruit NeoPixel Library
-void theaterChase(uint32_t c, uint8_t wait) {
-  for(uint16_t j=0; j<10; j++) {  //do 10 cycles of chasing
-    for(uint16_t k=0; k<3; k++) {
-      for(uint16_t i=0; i<strip.numPixels(); i+=3) {
-        strip.setPixelColor(i+k, c);    //turn every third pixel on
+// "Bin full" state variable
+boolean full = false;
+
+// Other variables
+int i = 0, k = 0;
+
+void loop() {
+  // ADC reads + 5 ms delay since we only get one value every 5 ms
+  ref = analogRead(A0);
+  sensor = analogRead(A1);
+  delay(5);
+
+  // Sensor: ADC counts -> voltage -> distance
+  value = voltage_to_distance(sensor * 5.0 / 1023.0);
+
+  // Time update
+  xm1 = x;
+  Pm1 = P + Q;
+
+  // Measurement update
+  K = Pm1 / (Pm1 + R);
+  x = xm1 + K*(value - xm1);
+  P = (1 - K)*Pm1;
+
+  // Reference: ADC counts -> voltage -> distance
+  value = voltage_to_distance(ref * 5.0 / 1023.0);
+  
+  // Bar graph + reference marker
+  for(i=0; i<8; i++) {
+    float level = 80 - 10*i;
+    if( x < level ) {
+      strip.setPixelColor(i, 0, 255, 0);
+    } else {
+      strip.setPixelColor(i, 0, 0, 0);
+    }
+
+    if( (value < level) && (value > level - 10) ) {
+      strip.setPixelColor(i+8, 255, 255, 0);
+    } else {
+      strip.setPixelColor(i+8, 0, 0, 0);
+    }
+  }
+
+  // Bin full condition + alert
+  // NOTE:  The full condition can only be cleared by a reset of the device
+  if( x <= value ) {
+    full = true;
+  }
+  if( full ) {
+    long j = millis() % 300;
+    for(i=0; i<8; i++) {
+      if( (j < 150) || (i % 2== k) ) {
+        strip.setPixelColor(i+32, 255, 255, 255);
+        strip.setPixelColor(i+24, 0, 0, 0);
+        strip.setPixelColor(i+16, 255, 255, 255);
+      } else {
+        strip.setPixelColor(i+32, 0, 0, 0);
+        strip.setPixelColor(i+24, 255, 255, 255);
+        strip.setPixelColor(i+16, 0, 0, 0);
       }
-      strip.show();
-      delay(wait);
-     
-      for(uint16_t i=0; i<strip.numPixels(); i+=3) {
-        strip.setPixelColor(i+k, 0);        //turn every third pixel off
+    }
+    if( j < 10 ) {
+      k++;
+      if( k == 2 ) {
+        k = 0;
       }
     }
   }
+  
+  strip.show();
 }
